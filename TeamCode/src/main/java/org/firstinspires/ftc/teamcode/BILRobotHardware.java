01a5840063984a74cbd2222a41a244816c5fc548
LorenzoPane <LorenzoPane@users.noreply.github.com>
2018-02-20 19:40:21 -0500

    Add new motors to init() (#27)
    * Release v3.6.
    
    * Remap joystick for new wheel layout
    
    * Add functionality for tele-op on new robot
    
    Added control for:
    -Fine movement control w/ left trigger on gamepad 1
    -New relic arm deployment and retraction w/ X and A on gamepad 2
    -New relic arm extention and retraction w/ right stick on gamepad 2
    -New glyph system lift and pitch w/ dpad Y and X, respectively gamepad 2
    -New glyph gatherer spinning w/ left stick on gamepad 2
    
    Still Todo:
    -Cryptobox, auto allignment
    -Auto ballance
    -Relic grabbing
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILRobotHardware.java
    #	modified:   TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILTeleOp.java
    #
    # Untracked files:
    #	DogeCV/
    #	openCVLibrary320/
    #
    
    * Add semicolon
    
    * Add new motors to Init()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILRobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILRobotHardware.java
@@ -65,6 +65,11 @@ public class BILRobotHardware {
         motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
         motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
         motorLift = hwMap.dcMotor.get("lift");
+        relicGrab = hwMap.servo.get("Relic_Claw");
+        relicExtend = hwMap.servo.get("Relic_Extend");
+        liftPitch = hwMap.servo.get("Lift_Pitch");
+        glyphGatherer = hwMap.dcMotor.get("glyph_gatherer");
+        relicDeploy = hwMap.servo.get("relic_deploy");
 
         //Lift & Extender Motors
         //motorLift = hwMap.dcMotor.get("lift");
