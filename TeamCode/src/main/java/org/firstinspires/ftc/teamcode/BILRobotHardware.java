8da136b394edb776114f05fb5cb1d8c26c6224bc
Mikaburrie <mikaburrie@gmail.com>
2018-03-22 20:33:22 -0400

    Fix issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILRobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILRobotHardware.java
@@ -63,7 +63,7 @@ public class BILRobotHardware {
         motorFrontLeft = hwMap.dcMotor.get("Left_Front");
         motorBackLeft = hwMap.dcMotor.get("Left_Back");
         motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
-        motorBackLeft.setDirection(DcMotor.Direction.REVERSE
+        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
                                    
         motorLift = hwMap.dcMotor.get("Lift");
         relicGrab = hwMap.servo.get("Relic_Claw");
